3b44515ec4fac1dffecaf99d5cad41a5eea2b3dc
nikeshmo <56961767+nikeshmo@users.noreply.github.com>
2020-08-31 22:05:48 +0200

    Endringer

--- src/AlgDataOving.java
+++ src/AlgDataOving.java
@@ -8,5 +8,24 @@ public class AlgDataOving {
         for (int i =1; i <= 10; i++){
             System.out.println(i);
         }
+
+        int [] a = {8,5,2,77,12,1,5,1};
+
+        //finne det indexen til det minste tallet
+        int min_index = min(a);
+        System.out.println(min_index);
+
+    }
+
+    static int min(int [] a) {
+        int min_index = 0;
+
+        for (int i=0; i<a.length; i++){
+            if (a[min_index]>a[i]){
+                min_index = i;
+            }
+        }
+
+        return min_index;
     }
 }
