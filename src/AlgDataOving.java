b71a4071d0aed4a28d4efd2c6a6b628a8ba895bc
nikeshmo <56961767+nikeshmo@users.noreply.github.com>
2020-08-31 15:00:16 +0200

    Initial commit

---
+++ src/AlgDataOving.java
@@ -0,0 +1,12 @@
+public class AlgDataOving {
+
+    public static void main(String[] args){
+        System.out.println("Hei AlgDat!");
+
+        System.out.println("Teller til 10");
+
+        for (int i =1; i <= 10; i++){
+            System.out.println(i);
+        }
+    }
+}
